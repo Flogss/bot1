4a35fc68a4d9746a0ed1e103f824753704a891a4
Flogss <flow221108@gmail.com>
2023-01-17 14:08:39 +0100

    Add files via upload

---
+++ les_deux.js
@@ -0,0 +1,27 @@
+const mineflayer = require('mineflayer')
+
+const bot = mineflayer.createBot({
+  host: 'DonutSMP.net', // minecraft server ip
+  username: 'shamchou94@gmail.com', // minecraft username
+  // password: 'xxx', // minecraft password, comment out if you want to log into online-mode=false servers  
+  port: 25565,                // only set if you need a port that isn't 25565
+  version: '1.18',            // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
+  auth: 'microsoft'              // only set if you need microsoft auth, then set this to 'microsoft'
+})
+mineflayer.createBot({
+  host: 'DonutSMP.net', // minecraft server ip
+  username: 'flow221108@gmail.com', // minecraft username
+  // password: 'xxx', // minecraft password, comment out if you want to log into online-mode=false servers  
+  port: 25565,                // only set if you need a port that isn't 25565
+  version: '1.18',            // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
+  auth: 'microsoft'              // only set if you need microsoft auth, then set this to 'microsoft'
+})
+
+bot.on('login', () => {
+  console.log("Bot connecté au serveur.");
+});
+
+
+
+bot.on('kicked', console.log)  //pour voir les erreur de kick
+bot.on('error', console.log)
